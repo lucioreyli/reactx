85bbe4e090047ee16804c30b3477874ce0354479
Lúcio Andrade <lucioandradejr@gmail.com>
2025-03-03 22:51:31 -0400

    feat: tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css';
-
 const App = () => {
   return (
     <div className="content">
-      <h1>Rsbuild with React</h1>
+      <h1 className="font-bold bg-red-500">Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
   );
